84af91b88f17013c07be39eed7b586f39ad622d9
Jeremy <jeremythegreat95@gmail.com>
2025-05-27 21:01:47 +0800

    build pagination function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import './App.css';
 import Data from './data.json';
+import { useState } from "react";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 5;
+  const lastIndex = currentPage * rowsPerPage;
+  const firstIndex = lastIndex - rowsPerPage;
+  const rows = Data.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(Data.length / rowsPerPage);
+  const totalPagesDisplay = [...Array(totalPages + 1).keys()].slice(1);
+
+  function previousPage() {
+    if (currentPage !== firstIndex + 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  function nextPage() {
+    if (currentPage !== totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function renderCurrentPage(page) {
+    setCurrentPage(page);
+  }
+
   return (
     <>
       <table className="table-auto">
@@ -13,7 +38,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {Data.map((data, i) => (
+          {rows.map((data, i) => (
             <tr key={i}>
               <td>{data.id}</td>
               <td>{data.name}</td>
@@ -23,8 +48,31 @@ function App() {
           ))}
         </tbody>
       </table>
+
+      <nav className="page-nav">
+        <ul className="page-items">
+          {currentPage <= firstIndex &&
+            <li className="page-item"
+              onClick={previousPage}>
+              prev
+            </li>}
+
+          {totalPagesDisplay.map((num, i) => (
+            <li className={`${currentPage === num ? "active" : ""} page-item`} key={i}
+              onClick={() => renderCurrentPage(num)}>
+              {num}
+            </li>
+          ))}
+
+          {currentPage !== totalPages &&
+            <li className="page-item"
+              onClick={nextPage}>
+              next
+            </li>}
+        </ul>
+      </nav>
     </>
   )
 }
 
-export default App
+export default App;
