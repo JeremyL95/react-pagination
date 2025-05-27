0d9ddcd9dbdd08fb1156cded3775e9bd98d44197
Jeremy <jeremythegreat95@gmail.com>
2025-05-27 16:02:00 +0800

    render table with dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import './App.css'
+import './App.css';
+import Data from './data.json';
 
 function App() {
-
   return (
     <>
-
+      <table className="table-auto">
+        <thead>
+          <tr>
+            {Object.keys(Data[0]).map((key, i) => (
+              <td key={`th${i}`}>{key}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {Data.map((data, i) => (
+            <tr key={i}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.email}</td>
+              <td>{data.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
