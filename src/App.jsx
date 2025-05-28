cc17d8652b885a4419c70729c5d1fd4d48dc7b5a
Jeremy <jeremythegreat95@gmail.com>
2025-05-28 09:35:45 +0800

    style table with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -28,50 +28,57 @@ function App() {
   }
 
   return (
-    <>
-      <table className="table-auto">
-        <thead>
+
+
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             {Object.keys(Data[0]).map((key, i) => (
-              <td key={`th${i}`}>{key}</td>
+              <th scope="col" className="px-6 py-3" key={`th${i}`}>{key}</th>
             ))}
           </tr>
         </thead>
+
         <tbody>
           {rows.map((data, i) => (
-            <tr key={i}>
-              <td>{data.id}</td>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.phone}</td>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200" key={i}>
+              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{data.id}</td>
+              <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{data.name}</td>
+              <td className="px-6 py-4">{data.email}</td>
+              <td className="px-6 py-4">{data.phone}</td>
             </tr>
           ))}
         </tbody>
       </table>
 
-      <nav className="page-nav">
-        <ul className="page-items">
-          {currentPage <= firstIndex &&
-            <li className="page-item"
-              onClick={previousPage}>
-              prev
-            </li>}
+      <nav aria-label="Page navigation example" className="flex justify-center mt-16">
+        <ul className="flex items-center -space-x-px h-10 text-base">
+          <li className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer"
+            onClick={previousPage}>
+            <span className="sr-only">Previous</span>
+            <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+            </svg>
+          </li>
 
           {totalPagesDisplay.map((num, i) => (
-            <li className={`${currentPage === num ? "active" : ""} page-item`} key={i}
+            <li className={`${currentPage === num ? "active" : ""} flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer`} key={i}
               onClick={() => renderCurrentPage(num)}>
               {num}
             </li>
           ))}
 
-          {currentPage !== totalPages &&
-            <li className="page-item"
-              onClick={nextPage}>
-              next
-            </li>}
+          <li className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer"
+            onClick={nextPage}>
+            <span className="sr-only">Next</span>
+            <svg className="w-3 h-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+            </svg>
+          </li>
         </ul>
       </nav>
-    </>
+    </div>
   )
 }
 
